d9d1594ec9ca4ec692d343b87deb50cfe9ac3277
Mina_Ashraf <minkir014@gmail.com>
2019-07-28 20:12:01 +0300

    Fixing probable issues

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes = require("./routes/comments"),
    campgroundRoutes = require("./routes/campgrounds"),
    indexRoutes = require("./routes/index")
 
-mongoose.connect(process.env.DATABASEURL, {
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
+mongoose.connect(url, {
    useNewUrlParser: true
 }).then(() => {
    console.log("Connected to DB!");;
